434751e19baa6b754e9cd74a03d6a776865ecad8
Baljinnyam07 <116425294+Baljinnyam07@users.noreply.github.com>
2023-09-28 16:57:36 +0800

    admin some fix

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -5,7 +5,7 @@ function Admin() {
   return (
     <div className="text-white flex h-screen">
       <AdminNav/>
-      <main className="flex-grow p-10 bg-[#e9ebf0]">
+      <main className="flex-grow p-10 bg-[#e9ebf0] ml-40">
         <h1 className="text-3xl font-semibold mb-6 text-[#454655]">Welcome To Admin</h1>
         <p className="text-lg text-[#454655]">
           This is your admin dashboard where you can manage various aspects of your application.
