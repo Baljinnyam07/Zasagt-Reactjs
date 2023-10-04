e4bfdd976bcd1fc6381765ac55a75695a42eb328
Baljinnyam07 <116425294+Baljinnyam07@users.noreply.github.com>
2023-10-04 14:06:22 +0800

    support minning fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -109,13 +109,13 @@ export default function Nav({lan}) {
 
     return (
         <>
-        <button className="text-white rounded-full pr-10 sm:hidden fontSomething" onClick={showMenu}>
+        <button className="text-white rounded-full pr-10 sm:hidden" onClick={showMenu}>
                 <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none">
                   <path d="M3 6H21M3 12H21M3 18H21" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
               </button>
 
-              <div id="menu" className={`fixed fontSomething top-0 w-full left-0 h-screen bg-[#23356B] z-50 hidden lg:hidden p-[20px] ${navbarBg ? "bg-[#050F36]" : ""}`}>
+              <div id="menu" className={`fixed top-0 w-full left-0 h-screen bg-[#23356B] z-50 hidden lg:hidden p-[20px] ${navbarBg ? "bg-[#050F36]" : ""}`}>
         <div className="flex justify-between items-center mb-[40px] mt-[-3px]">
         {lan === "mn" ? (
           <svg xmlns="http://www.w3.org/2000/svg" width="78" height="50" viewBox="0 0 78 50" fill="none">
@@ -254,7 +254,7 @@ export default function Nav({lan}) {
           </div>
         </div>
       </div>
-        <div className="flex items-center fontSomething">
+        <div className="flex items-center">
             <div className="hidden lg:block">
             <a href="/">
                 <div className={`menu-hover text-[14px] font-400 ${url === '/' ? 'text-[#D0A616]' : 'text-[#fff]'} lg:mx-4 uppercase`}>
